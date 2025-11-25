7e0f3da322cd82c254cc04d8154cbcdf26353a37
Hotties <changmin2580@naver.com>
2025-11-25 18:54:31 +0900

    stock_info, daily_price 클래스에 데이터 대입 성공, 그러나 속도측면에서 개선하였으나 여전히 느림, 멀티프로세싱 코려

--- data_collector.py
+++ data_collector.py
@@ -13,45 +13,61 @@ from db.data_class import stock_info, daily_price, daily_stats
 #     df = fdr.DataReader( "019175",start=end_date,end=end_date)
 
 df_krx = fdr.StockListing('KRX')
-stock_info_list = []
-daily_price_list = []
 daily_stats_list = []
 
 ## df_krx 순회 하면서 새로운 객체 생성 
 # (krx/code/name/)
 ## krx/code/name/close/date
 ## krx/code/name/close/5day/20day/60day/120day/volume
+df_krx_subset = df_krx[['Code', 'Name']]
 
-Codes = df_krx['Code'].tolist()
-Names = df_krx['Name'].tolist()
-stock_info_zip = list(zip(Codes,Names))
+stock_info_list = [
+    stock_info(
+        code = row.Code,
+        country = 'KRX',
+        name = row.Name
+    )
+    for row in df_krx_subset.itertuples()
+]
 
-for i,j in stock_info_zip:
-    stock_info_obj = stock_info(i,'KRX',j)
-    stock_info_list.append(stock_info_obj)
+
+print("complete making stock_info_list")
 
 end_date = datetime.now().strftime('%Y-%m-%d')
 start_date = (datetime.now()-timedelta(days=120)).strftime('%Y-%m-%d')
-df = fdr.DataReader( '000490',start=end_date,end=end_date)
-print(df)
-# for i in Codes:
-#     print(i)
-#     df = fdr.DataReader( i,start=start_date,end=end_date)
-#     df = df.reset_index()
-#     Date_list = df['Date'].tolist()
-#     Close_list = df['Close'].tolist()
-#     Volume_list = df['Volume'].tolist()
-#     daily_price_zip = list(zip(Date_list,Close_list,Volume_list))
-    
-#     for j,k,l in daily_price_zip:
-#         daily_price_obj = daily_price(i,j,k,l)
-#         daily_price_list.append(daily_price_obj)
 
-# for i in Codes:
-#     df = fdr.DataReader( i,start=start_date,end=end_date)
-#     df_close = {}
-#     df_close['MA5'] = df['Close'].rolling(window=5).mean()
-#     df_close['MA20'] = df['Close'].rolling(window=20).mean()
-#     df_close['MA60'] = df['Close'].rolling(window=60).mean()
-#     df_close['MA120'] = df['Close'].rolling(window=120).mean()
-#     print(df_close)
+all_daily_data_df = pd.DataFrame()
+
+for stock in stock_info_list:
+    df = fdr.DataReader( stock.code,start=start_date,end=end_date)
+    
+    df['Code'] = stock.code
+
+    df_subset = df[['Code', 'Close', 'Volume']]
+
+    all_daily_data_df = pd.concat([all_daily_data_df, df_subset])
+
+all_daily_data_df = all_daily_data_df.reset_index(names=['Date'])
+all_daily_data_df['Date_str'] = all_daily_data_df['Date'].dt.strftime('%Y-%m-%d')
+
+print("complete making all_daily_date_df")
+
+daily_price_list = [
+    daily_price(
+        code = row.Code,
+        date = row.Date_str,
+        close = row.Close,
+        volume = row.Volume
+    )
+    for row in all_daily_data_df.itertuples()
+]
+print(daily_stats_list)
+
+# # for i in Codes:
+# #     df = fdr.DataReader( i,start=start_date,end=end_date)
+# #     df_close = {}
+# #     df_close['MA5'] = df['Close'].rolling(window=5).mean()
+# #     df_close['MA20'] = df['Close'].rolling(window=20).mean()
+# #     df_close['MA60'] = df['Close'].rolling(window=60).mean()
+# #     df_close['MA120'] = df['Close'].rolling(window=120).mean()
+# #     print(df_close)
